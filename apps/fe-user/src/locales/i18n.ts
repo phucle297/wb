3875ffad46e23b4499c5c65ee4855945d4243a70
Le Dam Duy Phuc <somdp2907@gmail.com>
2024-02-16 14:31:56 +0700

    chore: fe-user: init i18n

--- apps/fe-user/src/locales/i18n.ts
+++ apps/fe-user/src/locales/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-import { TTranslationKeys } from "@/types/translation";
+import { TTranslationKeys } from "@wb/common/src/types/translation";
 
 import en from "./en.json";
 import vi from "./vi.json";
