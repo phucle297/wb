bf1bdcf1ec8587ceba7d7a760323c4305e87540d
Le Dam Duy Phuc <permees@nexondv.com>
2024-03-13 17:59:42 +0700

    fix: use mantine carousel instead of slick, add cllide-balls

--- apps/fe-user/src/components/carousel.tsx
+++ apps/fe-user/src/components/carousel.tsx
@@ -1,48 +1,21 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Carousel as MantineCarousel } from "@mantine/carousel";
+import { nanoid } from "@reduxjs/toolkit";
 import { ReactElement, ReactNode } from "react";
-import Slider from "react-slick";
+import "@mantine/carousel/styles.css";
 
 type Props = {
   data: ReactElement[] | ReactNode[];
   settings?: object;
 };
 
-const SamplePrevArrow = ({ onClick, type }: any) => {
-  if (type === "prev")
-    return (
-      <ChevronLeft
-        className="absolute left-0 top-1/2 z-40 -translate-y-1/2 cursor-pointer transition-all duration-300 hover:text-[hsl(var(--primary))]"
-        size={32}
-        onClick={onClick}
-      />
-    );
-  return (
-    <ChevronRight
-      className="absolute right-0 top-1/2 z-40 -translate-y-1/2 cursor-pointer transition-all duration-300 hover:text-[hsl(var(--primary))]"
-      size={32}
-      onClick={onClick}
-    />
-  );
-};
-
-const Carousel = ({ data, settings: settingsDefault }: Props) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <SamplePrevArrow type={"next"} />,
-    prevArrow: <SamplePrevArrow type={"prev"} />,
-    ...settingsDefault,
-  };
+const Carousel = ({ data }: Props) => {
   return (
     <div className="relative w-full">
-      <Slider {...settings}>{data}</Slider>
+      <MantineCarousel withIndicators loop>
+        {data.map((item) => {
+          return <MantineCarousel.Slide key={nanoid()}>{item}</MantineCarousel.Slide>;
+        })}
+      </MantineCarousel>
     </div>
   );
 };
